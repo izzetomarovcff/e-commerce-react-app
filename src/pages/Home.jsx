fb0b2261c0ff0c0a2e7e52ca9310b6c779049b69
izzetomarovcff <izzetomarovcff@gmail.com>
2024-02-13 01:58:59 +0400

    home page sign out

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { onAuthStateChanged, signOut } from 'firebase/auth'
+import { auth } from '../firebase'
 import { Link } from 'react-router-dom'
 function Home() {
+  const [authUser, setAuthUser] = useState()
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user)
+
+      } else {
+        setAuthUser(null)
+        window.location.href = `/signup`
+      }
+    })
+    return () => {
+      listen()
+    }
+  },[])
+  const userSignOut = () =>{
+    signOut(auth).then(()=>{
+        console.log("Sigin Out Successfully")
+    }).catch((error)=>{
+        console.log(error)
+    })
+}
   return (
     <div className='homepage'>
       <h1 className='mx-5 text-center text-primary border border-primary rounded p-3'>Home page will be here soon</h1>
-      <Link to="/signup" className='btn btn-primary mt-3 mb-5'>Sign Up Now</Link>
+      <button className='btn btn-primary mt-3 mb-5' onClick={userSignOut}>Sign Out</button>
     </div>
   )
 }
