f0b8bc9244aaec4eb3e12ba5a99b1f308405bc9c
izzetomarovcff <izzetomarovcff@gmail.com>
2024-02-14 12:59:17 +0400

    home update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,26 +11,23 @@ function Home() {
         setAuthUser(user)
       } else {
         setAuthUser(null)
-        window.location.href ="/login"
+        window.location.href = "/login"
       }
     })
     return () => {
       listen()
     }
-  },[])
-  const userSignOut = () =>{
-    signOut(auth).then(()=>{
-        console.log("Sigin Out Successfully")
-    }).catch((error)=>{
-        console.log(error)
-    })
-}
+  }, [])
+
   return (
     <div className='homepage'>
-      <h1 className='mx-5 text-center text-primary border border-primary rounded p-3'>Home page will be here soon</h1>
-      {authUser?(<h1 className='mx-5 text-center text-primary border border-primary rounded p-3'>You Logged In As: {authUser.email}</h1>):(null)}
-      {authUser? (<button className='btn btn-primary mt-3 mb-5' onClick={userSignOut}>Sign Out</button>):(null)}
-      <Footernavbar/>
+      <div className='checksale'>
+        <div className='checkgroup'>
+          <h1 className='mb-3'>Fashion<br></br>Sale</h1>
+          <button className='btn btn-primary px-5'>Check</button>
+        </div>
+      </div>
+      <Footernavbar />
     </div>
   )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Footernavbar from '../components/Footernavbar'
+import { onAuthStateChanged, signOut } from 'firebase/auth'
+import { auth } from '../firebase'
 
 function Profile() {
+  const [authUser, setAuthUser] = useState(null)
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      // console.log(user.email) //check token status
+      if (user) {
+        setAuthUser(user)
+      } else {
+        setAuthUser(null)
+        window.location.href = "/login"
+      }
+    })
+    return () => {
+      listen()
+    }
+  }, [])
+  const userSignOut = () => {
+    signOut(auth).then(() => {
+      console.log("Sigin Out Successfully")
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
   return (
-    <div><Footernavbar/></div>
+    <div>
+      {authUser ? (<h1 className='mx-5 text-center text-primary border border-primary rounded p-3'>You Logged In As: {authUser.email}</h1>) : (null)}
+      {authUser ? (<button className='btn btn-primary mt-3 mb-5' onClick={userSignOut}>Sign Out</button>) : (null)}
+      <Footernavbar />
+    </div>
   )
 }
 
