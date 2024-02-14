0186e29564055575c575375b06f55c8069ac3425
izzetomarovcff <izzetomarovcff@gmail.com>
2024-02-14 14:35:25 +0400

    home update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { onAuthStateChanged, signOut } from 'firebase/auth'
 import { auth } from '../firebase'
 import Footernavbar from '../components/Footernavbar'
+import { Link } from 'react-router-dom'
 function Home() {
   const [authUser, setAuthUser] = useState()
   useEffect(() => {
@@ -24,9 +25,94 @@ function Home() {
       <div className='checksale'>
         <div className='checkgroup'>
           <h1 className='mb-3'>Fashion<br></br>Sale</h1>
-          <button className='btn btn-primary px-5'>Check</button>
+          <a href='#salesection' className='btn btn-primary px-5'>Check</a>
+        </div>
+
+      </div>
+      <div id='salesection' className='mt-5'>
+        <h1 className='px-3'>Sale</h1>
+        <p className='px-3'>Super Summer Sale</p>
+        <div className='saleproduct '>
+
+          <div className="product mx-3" >
+            <div className='productimg'>
+              <img src="image/home/sale/sale1.svg" alt="product" />
+              <div className='saleper bg-primary'>-20%</div>
+            </div>
+            <div className='d-flex mt-2'>
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <p className='mb-0 fs-6'>(10)</p>
+            </div>
+            <p className='mb-1 mt-1'>Brend Name</p>
+            <h6>Product Name</h6>
+            <div className='d-flex'><del >15$</del><p className='text-primary ms-2 newprice'>12$</p></div>
+            <div className='addfav shadow-sm'><img src="image/home/sale/heart.svg" alt="" /></div>
+          </div>
+          <div className="product mx-3" >
+            <div className='productimg'>
+              <img src="image/home/sale/sale1.svg" alt="product" />
+              <div className='saleper bg-primary'>-20%</div>
+            </div>
+            <div className='d-flex mt-2'>
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <p className='mb-0 fs-6'>(10)</p>
+            </div>
+            <p className='mb-1 mt-1'>Brend Name</p>
+            <h6>Product Name</h6>
+            <div className='d-flex'><del >15$</del><p className='text-primary ms-2 newprice'>12$</p></div>
+            <div className='addfav shadow-sm'><img src="image/home/sale/heart.svg" alt="" /></div>
+          </div>
+          <div className="product mx-3" >
+            <div className='productimg'>
+              <img src="image/home/sale/sale1.svg" alt="product" />
+              <div className='saleper bg-primary'>-20%</div>
+            </div>
+            <div className='d-flex mt-2'>
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <p className='mb-0 fs-6'>(10)</p>
+            </div>
+            <p className='mb-1 mt-1'>Brend Name</p>
+            <h6>Product Name</h6>
+            <div className='d-flex'><del >15$</del><p className='text-primary ms-2 newprice'>12$</p></div>
+            <div className='addfav shadow-sm'><img src="image/home/sale/heart.svg" alt="" /></div>
+          </div>
+          <div className="product mx-3" >
+            <div className='productimg'>
+              <img src="image/home/sale/sale1.svg" alt="product" />
+              <div className='saleper bg-primary'>-20%</div>
+            </div>
+            <div className='d-flex mt-2'>
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <img src="image/home/sale/star.svg" alt="" />
+              <p className='mb-0 fs-6'>(10)</p>
+            </div>
+            <p className='mb-1 mt-1'>Brend Name</p>
+            <h6>Product Name</h6>
+            <div className='d-flex'><del >15$</del><p className='text-primary ms-2 newprice'>12$</p></div>
+            <div className='addfav shadow-sm'><img src="image/home/sale/heart.svg" alt="" /></div>
+          </div>
+          
+          
+
+
         </div>
       </div>
+
       <Footernavbar />
     </div>
   )
